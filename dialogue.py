55d4de2047d64fe3dbca97568065c3c08ef90f0d
sangeun0 <80662666+sangeun0@users.noreply.github.com>
2021-03-15 13:08:23 +0900

    2주차 수업시간

---
+++ dialogue.py
@@ -0,0 +1,16 @@
+name = input( "이름" )
+print( " ", name ," 님 반갑습니다. ")
+age= input( "age" )
+print( " 10년 후면 ",int(age) + 10," 살이 되시는군요! ")
+
+message = ('ㅎㅎ"ㅇㅇ"')
+print( message )
+
+print( "철수가 \"안녕\" 이라고 말했다." )
+        #escape sequence는 문자열 안에서만 쓴다 ",'
+        #\ \ 문자열 안 문자열을 인용구로 쓰고 싶을때 
+        #\t  공백(tab)을 만들어줘!! 
+        #\n  뒤 문장부터 줄바꿈
+
+        #''' ''' 여러줄 문자열 표현할 때
+
