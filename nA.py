4cfca9107dd652452c604b94865e1c4263311e37
sangeun0 <80662666+sangeun0@users.noreply.github.com>
2021-03-22 13:16:24 +0900

    Update nA.py

--- nA.py
+++ nA.py
@@ -1,5 +1,5 @@
-answer1= int( 91.44*2.1 )
+answer1= float( 91.44*2.1 )
 print("2.1yd", "=",round(answer1,1),"cm" )
 
-answer2= ( 10.5*2.54 )
+answer2= float( 10.5*2.54 )
 print("10.5in", "=" , round(answer2,1),"cm")
