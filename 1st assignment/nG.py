9ebde8de717eb2120f083f0ac15810cc7922de1d
sangeun0 <80662666+sangeun0@users.noreply.github.com>
2021-04-25 17:56:30 +0900

    Update nG.py

--- 1st assignment/nG.py
+++ 1st assignment/nG.py
@@ -2,6 +2,7 @@ A= list(input("enter the string: "))
 B= list(input("enter the string: "))
 n= int(input("enter the number: "))
 
-del[B[0:n]]
+del[B[0:n]] #원하는 인덱스 제거해주는 함수 del
+            #0부터 n까지 인덱싱
 string= A[0:n]+B
 print(string)
