5e5d11d4f69c458052cf57485f007998422c3dd1
sangeun0 <80662666+sangeun0@users.noreply.github.com>
2021-04-26 23:51:18 +0900

    Update nnf.py

--- 2nd assignment/nnf.py
+++ 2nd assignment/nnf.py
@@ -1,7 +1,7 @@
 number1 = 35
 number2 = 10
 
-quo = ( number1//number2 )
-rem = ( number1%number2 )
+quo = ( number1//number2 ) #몫
+rem = ( number1%number2 ) #나머지
 
 print( int(number1) ,"/",  int(number2) ,"=",  quo , "."*3 , rem )
