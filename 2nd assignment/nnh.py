7059b64eacc1dfd56b5aa8f8313a1b5063d5d4be
sangeun0 <80662666+sangeun0@users.noreply.github.com>
2021-04-26 23:52:38 +0900

    Update nnh.py

--- 2nd assignment/nnh.py
+++ 2nd assignment/nnh.py
@@ -3,7 +3,7 @@ minSuWeight = 35
 kiYoungTall = 145
 kiYoungWeight = 35
 
-if (minSuTall>kiYoungTall) and (minSuWeight>kiYoungWeight):
+if (minSuTall>kiYoungTall) and (minSuWeight>kiYoungWeight): #민수가 키도 크고 몸무게도 크다= 만족하려면 and 사용
     print("true")
 else :
     print("false")
