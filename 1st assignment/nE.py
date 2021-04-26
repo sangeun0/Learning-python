85d4fbaedf3e2691e2c0a2b38f2035d621caa2c5
sangeun0 <80662666+sangeun0@users.noreply.github.com>
2021-04-26 23:48:14 +0900

    Update nE.py

--- 1st assignment/nE.py
+++ 1st assignment/nE.py
@@ -2,6 +2,6 @@ string= input( "Enter an input: " )
 number= input( "Enter the number: " ) 
 
 ans=list(string)
-print(ans.pop(int(number)))
+print(ans.pop(int(number))) #index의 값 제거 = pop 함수
 print(ans)
 
